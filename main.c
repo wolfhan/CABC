cb40513595cdabba8b0bb9700baec757e038bc92
wolfhan <wolfwolfhan@gmail.com>
2020-05-14 10:36:49 +0800

    Add shrink blanks function

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int countEscapeSequences();
+void shrinkBlanks();
 
 int main()
 {
-  int number = countEscapeSequences();
+  //int number = countEscapeSequences();
+  shrinkBlanks();
   return 0;
 }
 
@@ -18,4 +20,25 @@ int countEscapeSequences()
 	  ++number;
   printf("%d\n", number);
   return number;
+}
+
+/* Replace more than one blanks by single blank */
+void shrinkBlanks()
+{
+  int c, number = 0;
+  while ((c = getchar()) != EOF)
+  {
+      if (c == ' ')
+      {
+	++number;
+      }
+      else
+      {
+	number = 0;
+      }
+      if (number <= 1)
+      {
+	putchar(c);
+      }
+  }
 }
