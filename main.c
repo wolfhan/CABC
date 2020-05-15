52fc530cb5d995c20f65670d848794b8f06c2d88
wolfhan <wolfwolfhan@gmail.com>
2020-05-15 21:26:58 +0800

    Add word count program

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
+
 int countEscapeSequences();
 void shrinkBlanks();
 void printEscapeSequences();
+void countLineWordChar();
 
 int main()
 {
   //int number = countEscapeSequences();
   //shrinkBlanks();
-  printEscapeSequences();
+  //printEscapeSequences();
+  countLineWordChar();
   return 0;
 }
 
@@ -66,4 +71,25 @@ void printEscapeSequences()
 	    putchar(c);
 	}
     }
+}
+
+/* count lines, words, and characters in input */
+void countLineWordChar()
+{
+    int c, nl, nw, nc, state;
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF)
+    {
+	++nc;
+	if (c == '\n')
+	    ++nl;
+	if (c == ' ' || c == '\n' || c == '\t')
+	    state = OUT;
+	else if (state == OUT) {
+	    state = IN;
+	    ++nw;
+	}
+    }
+    printf("%d %d %d\n", nl, nw, nc);
 }
