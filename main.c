40cc9540f658bc8591f0e3b626bb6cc710121b73
wolfhan <wolfwolfhan@gmail.com>
2020-09-25 15:36:57 +0800

    comment some lines of main.c

--- main.c
+++ main.c
@@ -34,7 +34,7 @@ int main()
     //printCharacterFrequencies();
     //printChars();
     //printGt80Chars();
-    reversePolishCalculator();
+    //reversePolishCalculator();
     return 0;
 }
 
@@ -272,40 +272,40 @@ int getlinechar(char s[], int lim)
     return i;
 }
 
-void reversePolishCalculator()
-{
-    int type;
-    double op2;
-    char s[MAXOP];
-
-    while ((type = getop(s)) != EOF) {
-        switch (type) {
-            case NUMBER:
-                push(atof(s));
-                break;
-            case '+':
-                push(pop() + pop());
-                break;
-            case '*':
-                push(pop() * pop());
-                break;
-            case '-':
-                op2 = pop();
-                push(pop() - op2);
-                break;
-            case '/':
-                op2 = pop();
-                if (op2 != 0.0)
-                    push(pop() / op2);
-                else
-                    printf("error: zero divisor\n");
-                break;
-            case '\n':
-                printf("\t%.8g\n", pop());
-                break;
-            default:
-                printf("error: unknown command %s\n", s);
-                break;
-        }       
-    }
-}
+//void reversePolishCalculator()
+//{
+//    int type;
+//    double op2;
+//    char s[MAXOP];
+//
+//    while ((type = getop(s)) != EOF) {
+//        switch (type) {
+//            case NUMBER:
+//                push(atof(s));
+//                break;
+//            case '+':
+//                push(pop() + pop());
+//                break;
+//            case '*':
+//                push(pop() * pop());
+//                break;
+//            case '-':
+//                op2 = pop();
+//                push(pop() - op2);
+//                break;
+//            case '/':
+//                op2 = pop();
+//                if (op2 != 0.0)
+//                    push(pop() / op2);
+//                else
+//                    printf("error: zero divisor\n");
+//                break;
+//            case '\n':
+//                printf("\t%.8g\n", pop());
+//                break;
+//            default:
+//                printf("error: unknown command %s\n", s);
+//                break;
+//        }       
+//    }
+//}
